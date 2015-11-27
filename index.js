5009106af124dba46f054d3e2e910b0b131a2d8e
Anand Thakker <vestibule@anandthakker.net>
2015-11-27 14:40:34 -0500

    Fix lint errors

--- index.js
+++ index.js
@@ -1,93 +1,87 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-  url = require('url'),
-  path = require('path'),
-  trumpet = require('trumpet'),
-  through = require('through2'),
-  next = require('next-stream'),
-  duplexer = require('duplexer2'),
-  request = require('request');
+var fs = require('fs')
+var url = require('url')
+var path = require('path')
+var trumpet = require('trumpet')
+var through = require('through2')
+var next = require('next-stream')
+var duplexer = require('duplexer2')
+var request = require('request')
 
-module.exports = stylestream;
+module.exports = stylestream
 
-var reHttp = /^http/;
+var reHttp = /^http/
 
-function stylestream(opts) {
-  var horn = trumpet(),
-    styles = next([]),
-    pending = 0;
+function stylestream (opts) {
+  var horn = trumpet()
+  var styles = next([])
+  var pending = 0
 
-  opts = opts || {
-    baseurl: null
-  }
+  opts = opts || { baseurl: null }
 
-  if(reHttp.test(opts.url)) {
+  if (reHttp.test(opts.url)) {
     return request(opts.url).pipe(stylestream({
       baseurl: opts.url,
       basepath: null
-    }));
+    }))
   }
 
-  horn.selectAll('link', function(elem) {
-    elem.getAttributes(function(attributes) {
-      if('stylesheet' === ((attributes.rel || '').toLowerCase()) ||
-        ('undefined' === typeof attributes.rel) &&
-        attributes.href)
-      try {
-        pushStreamForLocation(attributes.href);
-      } catch (e) {
-        styles.emit('error', e);
+  horn.selectAll('link', function (elem) {
+    elem.getAttributes(function (attributes) {
+      if (((attributes.rel || '').toLowerCase() === 'stylesheet') ||
+        (typeof attributes.rel === 'undefined') && attributes.href) {
+        try {
+          pushStreamForLocation(attributes.href)
+        } catch (e) {
+          styles.emit('error', e)
+        }
       }
-    });
-  });
+    })
+  })
 
-  horn.selectAll('style', function(elem) {
-    styles.push(elem.createReadStream());
-  });
-  
-  horn.on('end', closeStyles);
+  horn.selectAll('style', function (elem) {
+    styles.push(elem.createReadStream())
+  })
 
-  return duplexer(horn, styles);
+  horn.on('end', closeStyles)
 
+  return duplexer(horn, styles)
 
-  function pushStreamForLocation(loc) {
-    var isHttp = reHttp.test(loc);
+  function pushStreamForLocation (loc) {
+    var isHttp = reHttp.test(loc)
     if (reHttp.test(opts.baseurl)) {
-      isHttp = true;
-      loc = url.resolve(opts.baseurl, loc);
+      isHttp = true
+      loc = url.resolve(opts.baseurl, loc)
     }
     if (isHttp) {
-      pending++;
+      pending++
       var stream = request({
         method: 'GET',
         uri: loc,
         gzip: true
-      }).pipe(through());
-      stream.on('end', closeStyles);
-      styles.push(stream);
+      }).pipe(through())
+      stream.on('end', closeStyles)
+      styles.push(stream)
     }
-    else if (opts.basepath) styles.push(fs.createReadStream(path.resolve(opts.basepath, loc)));
+    else if (opts.basepath) styles.push(fs.createReadStream(path.resolve(opts.basepath, loc)))
     else if (typeof opts.basepath === 'undefined') throw new Error('Cannot resolve href without a baseurl or basepath.')
   }
-  
-  function closeStyles() {
-    if(pending-- <= 0) {
-      styles.close();
+
+  function closeStyles () {
+    if (pending-- <= 0) {
+      styles.close()
     }
   }
 }
 
-
-
 if (require.main === module) {
-  if(process.stdin.isTTY) {
+  if (process.stdin.isTTY) {
     stylestream({url: process.argv[2]})
-    .pipe(process.stdout);
-  }
-  else {
+      .pipe(process.stdout)
+  } else {
     process.stdin
-    .pipe(stylestream({basepath: process.argv[2]}))
-    .pipe(process.stdout);
+      .pipe(stylestream({basepath: process.argv[2]}))
+      .pipe(process.stdout)
   }
 }
--- test/base.js
+++ test/base.js
@@ -1,45 +1,44 @@
-
 var fs = require('fs'),
-    test = require('tape'),
-    concat = require('concat-stream'),
-    stylestream = require('../')
-    
-test('resolving <link> element', function(t) {
+  test = require('tape'),
+  concat = require('concat-stream'),
+  stylestream = require('../')
+
+test('resolving <link> element', function (t) {
   fs.createReadStream(__dirname + '/link.html')
-  .pipe(stylestream({basepath: __dirname}))
-  .pipe(concat({encoding: 'string'}, function(data) {
-    t.equal(data, fs.readFileSync(__dirname + '/link.css').toString())
-    t.end()
-  }))
+    .pipe(stylestream({basepath: __dirname}))
+    .pipe(concat({encoding: 'string'}, function (data) {
+      t.equal(data, fs.readFileSync(__dirname + '/link.css').toString())
+      t.end()
+    }))
 })
 
-test('<style> element', function(t) {
+test('<style> element', function (t) {
   fs.createReadStream(__dirname + '/style.html')
-  .pipe(stylestream({basepath: __dirname}))
-  .pipe(concat({encoding: 'string'}, function(data) {
-    t.equal(data.trim(), '.container { width: 900px; margin: 0 auto; }')
-    t.end()
-  }))
+    .pipe(stylestream({basepath: __dirname}))
+    .pipe(concat({encoding: 'string'}, function (data) {
+      t.equal(data.trim(), '.container { width: 900px; margin: 0 auto; }')
+      t.end()
+    }))
 })
 
-test('error with no basepath', function(t) {
+test('error with no basepath', function (t) {
   fs.createReadStream(__dirname + '/link.html')
-  .pipe(stylestream({}))
-  .on('error', function (e) {
-    t.ok(e)
-    t.end()
-  })
-  .resume()
+    .pipe(stylestream({}))
+    .on('error', function (e) {
+      t.ok(e)
+      t.end()
+    })
+    .resume()
 })
 
-test('silently skip link when basepath: false', function(t) {
+test('silently skip link when basepath: false', function (t) {
   fs.createReadStream(__dirname + '/link.html')
-  .pipe(stylestream({ basepath: false }))
-  .on('error', function (e) {
-    t.error(e)
-  })
-  .on('data', function (d) {
-    t.error(d)
-  })
-  .on('end', function () { t.end() })
+    .pipe(stylestream({ basepath: false }))
+    .on('error', function (e) {
+      t.error(e)
+    })
+    .on('data', function (d) {
+      t.error(d)
+    })
+    .on('end', function () { t.end() })
 })
